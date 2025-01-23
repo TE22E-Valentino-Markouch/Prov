9009378a0f74cf4439b59e1faf1dcc58f1919d63
Valentino Markouch <Valentino.Markouch@elev.ga.ntig.se>
2025-01-23 11:20:27 +0100

    Han inte mer

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,46 @@
+import java.security.PublicKey;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+        Intro();
 
 
     }
 
 
+    public static void Intro() {
+        System.out.println("Välj vilken typp av superhjälte du vill vara:");
+        System.out.println("1. Mutant");
+        System.out.println("2. Alien");
+        System.out.println("3. Ronnie Coleman");
+        System.out.println("Skriv den siffran som tillhör superhjälten du vill vara.");
+        while (true){
+            Scanner myObj = new Scanner(System.in);
 
-public void RonnieColeman() {
+            int HeroVal = Integer.parseInt(myObj.nextLine());
+            if (HeroVal == 1){
+            System.out.println("Du valde: Mutant");
+            break;
+            } else if (HeroVal == 2) {
+                System.out.println("Du valde: Alien");
+                break;
+            } else if (HeroVal == 3) {
+                System.out.println("Du valde Ronnie Coleman");
+                break;
+            }
+            else {
+                System.out.println("Skriv endast en siffra! Försök igen");
+            }
+        }
+    }
 
+    //  Hero type 1
+    public static void RonnieColeman() {
+
+    //  Stats:
     String HeroName = "RonnieColeman";
     int HP = 1000;
     int Power = 300;
@@ -17,8 +48,10 @@ public void RonnieColeman() {
 
     }
 
-    public void Mutant() {
+    //  Hero type 2
+    public static void Mutant() {
 
+        //  Stats:
         String HeroName = "Lizardman";
         int HP = 500;
         int Power = 200;
@@ -27,9 +60,10 @@ public void RonnieColeman() {
     }
 
 
+    //  Hero type 3
+    public static void Alien() {
 
-    public void Alien() {
-
+        //  Stats:
        String HeroName = "Superman";
         int HP = 700;
         int Power = 400;
