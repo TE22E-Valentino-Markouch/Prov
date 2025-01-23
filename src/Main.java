05805007876ef0de390fa62fffd0c0a09fbcac29
Valentino Markouch <Valentino.Markouch@elev.ga.ntig.se>
2025-01-23 10:53:22 +0100

    Finnat upp statsen

--- src/Main.java
+++ src/Main.java
@@ -10,15 +10,32 @@ public class Main {
 
 public void RonnieColeman() {
 
-
+    String HeroName = "RonnieColeman";
+    int HP = 1000;
+    int Power = 300;
+    String UnikeAttribute = "Lightweightbby";
 
     }
 
-    public void Mutant() {}
+    public void Mutant() {
+
+        String HeroName = "Lizardman";
+        int HP = 500;
+        int Power = 200;
+        String UnikeAttribute = "Poison";
+
+    }
 
 
 
-    public void Alien() {}
+    public void Alien() {
+
+       String HeroName = "Superman";
+        int HP = 700;
+        int Power = 400;
+        String UnikeAttribute = "Lazer";
+
+    }
 
 
 
