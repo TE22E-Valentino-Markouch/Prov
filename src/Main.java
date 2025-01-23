d04357cc928370d14de2c00cec2babacc2fdac71
Valentino Markouch <Valentino.Markouch@elev.ga.ntig.se>
2025-01-23 10:48:28 +0100

    Lagt in de första hjältarna

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello, World!");
+
+
     }
+
+
+
+public void RonnieColeman() {
+
+
+
+    }
+
+    public void Mutant() {}
+
+
+
+    public void Alien() {}
+
+
+
 }
